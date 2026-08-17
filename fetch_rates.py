6dabed37749d90bf80238d99fe99077f8da04b39
a1devproject <carlito.concha@gmail.com>
2026-08-17 10:08:52 +0800

    Create fetch_rates.py
    Script to fetch and save the rates

---
+++ fetch_rates.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import os
+
+API_KEY = os.environ["EXCHANGE_API_KEY"]
+url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+
+response = requests.get(url)
+data = response.json()
+
+output = {
+    "base": data["base_code"],
+    "rates": data["conversion_rates"],
+    "updated": data["time_last_update_utc"]
+}
+
+with open("rates.json", "w") as f:
+    json.dump(output, f, indent=2)
